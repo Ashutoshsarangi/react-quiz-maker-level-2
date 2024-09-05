213d217fc4d6c7f2c3f6a2a45f5c9e9a99c8786c
Ashu <ashutoshsarangi95@gmail.com>
2024-09-05 18:41:28 +0300

    WIP Deploy into github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-quiz-maker-level-2/",
   plugins: [react()],
   resolve: {
     alias: {},
